0d6f571f4fbfa75d56388a9e1a4ed6c2dca32954
Yusuf <teajhaney@gmail.com>
2025-09-04 03:54:08 +0100

    added types for products schemas and zod validations

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import type { JwtPayload } from 'jsonwebtoken';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export interface User extends Document {
   firstName: string;
@@ -20,4 +20,61 @@ export interface TokenPayload extends JwtPayload {
 
 export interface Request {
   user?: string | JwtPayload;
+}
+
+//product
+export interface Product extends Document {
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  sku: string;
+  category: Types.ObjectId;
+  brand: Types.ObjectId;
+  gender: Types.ObjectId;
+  images: Types.ObjectId[];
+  reviews: Types.ObjectId[];
+}
+
+//produc
+export interface Category extends Document {
+  name: string;
+}
+//brand
+export interface Brand extends Document {
+  name: string;
+}
+//gender
+export interface Gender extends Document {
+  name: 'men' | 'women' | 'unisex';
+}
+//product image
+export interface ProductImage extends Document {
+  url: string;
+  publicId: string;
+  productId: Types.ObjectId;
+}
+//Review
+export interface Review extends Document {
+  rating: number;
+  comment: string;
+  userId: Types.ObjectId;
+  productId: Types.ObjectId;
+}
+
+//cart
+export interface Cart extends Document {
+  userId: Types.ObjectId;
+  productId: Types.ObjectId;
+  quantity: number;
+}
+
+//order
+export interface Order extends Document {
+  userId: Types.ObjectId;
+  productId: Types.ObjectId;
+  totalPrice: number;
+  shippingAddress: object;
+  status: 'pending' | 'processing' | 'shipped' | 'delivered' | 'cancelled';
+  quantity: number;
 }
--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose';
 import { z } from 'zod';
 
 export const signupSchema = z.object({
@@ -11,4 +12,108 @@ export const signupSchema = z.object({
 export const signinSchema = z.object({
   email: z.email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+//zod schema for product validation
+export const productSchemaZod = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  description: z.string().min(10, 'Description must be at least 10 characters'),
+  price: z.number().positive(),
+  stock: z.number().int().min(0),
+  sku: z.string().min(5),
+  category: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  brand: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  gender: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  images: z
+    .array(
+      z.string().refine(val => Types.ObjectId.isValid(val), {
+        message: 'Invalid ObjectId',
+      })
+    )
+    .optional(),
+  reviews: z
+    .array(
+      z.string().refine(val => Types.ObjectId.isValid(val), {
+        message: 'Invalid ObjectId',
+      })
+    )
+    .optional(),
+});
+
+// Zod schema for category validation
+export const categorySchemaZod = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters'),
+});
+
+// Zod schema for brand validation
+export const brandSchemaZod = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters'),
+});
+
+// Zod schema for gender validation
+export const genderSchemaZod = z.object({
+  name: z.enum(['men', 'women', 'unisex']),
+});
+
+// Zod schema for product image validation
+export const productImageSchemaZod = z.object({
+  url: z.url(),
+  publicId: z.string().min(1),
+  productId: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+});
+
+// Zod schema for review validation
+export const reviewSchemaZod = z.object({
+  rating: z.number().int().min(1).max(5),
+  comment: z.string().optional(),
+  user: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  productId: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+});
+
+// Zod schema for cart validation
+export const cartSchemaZod = z.object({
+  userId: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  productId: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  quantity: z.number().positive(),
+});
+
+// Zod schema for order validation
+export const orderSchemaZod = z.object({
+  userId: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  productId: z.string().refine(val => Types.ObjectId.isValid(val), {
+    message: 'Invalid ObjectId',
+  }),
+  totalPrice: z.number().positive(),
+  shippingAddress: z.object({
+    street: z.string().min(3, 'Street must be at least 3 characters'),
+    city: z.string().min(2, 'City must be at least 2 characters'),
+    postalCode: z.string().min(4, 'Postal code must be at least 4 characters'),
+    country: z.string().min(2, 'Country must be at least 2 characters'),
+  }),
+  status: z.enum([
+    'pending',
+    'processing',
+    'shipped',
+    'delivered',
+    'cancelled',
+  ]),
+  quantity: z.number().int().min(1),
 });
